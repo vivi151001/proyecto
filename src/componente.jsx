f89011f17dd1c48a22ab7d0fa0bec79c582b2b0e
Nika <ccnico8@gmail.com>
2023-11-08 19:31:40 -0500

    Version 1 de Viviana

---
+++ src/componente.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const componente = () => {
+  return (
+    <div>
+      BIENVENIDOS
+      VIVIANA ALLISON RIVERA ANEIRA 
+      08 DE NOVIEMBRE DEL 2023
+      TURNO NOCHE
+    </div>
+  )
+}
+
+export default componente
