fa644b2313daa3e3e3fc1a317fc5aa605d39c698
Viviana <ccnico8@gmail.com>
2023-11-08 19:44:05 -0500

    Version 1 de Viviana

---
+++ src/Componentes/Componente.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Componente = () => {
+  return (
+    <div>
+      <h1>BIENVENIDOS</h1>
+      <h1>VIVIANA ALLISON RIVERA ANEIRA</h1> 
+      <h1>08 DE NOVIEMBRE DEL 2023</h1>
+      <h1>TURNO NOCHE</h1>
+    </div>
+  )
+}
+
+export default Componente;
