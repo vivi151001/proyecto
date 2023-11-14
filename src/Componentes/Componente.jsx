35eadbc3ccc587ac78e3eea738f8e5c3f5d29052
vivi151001 <137512572+vivi151001@users.noreply.github.com>
2023-11-14 16:06:24 -0500

    Update Componente.jsx

--- src/Componentes/Componente.jsx
+++ src/Componentes/Componente.jsx
@@ -4,8 +4,8 @@ const Componente = () => {
   return (
     <div>
       <h1>BIENVENIDOS</h1>
-      <h1>VIVIANA ALLISON RIVERA ANEIRA</h1> 
-      <h1>08 DE NOVIEMBRE DEL 2023</h1>
+      <h1>INSTITUTO SUPERIOR TECNOLOGICO HONORIO DELGADO ESPINOZA</h1>
+      <h1>VIVIANA ALLISON RIVERA ANEIRA</h1>
       <h1>TURNO NOCHE</h1>
     </div>
   )
