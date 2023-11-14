d49adb23a9d846332e088ade3943c58a3dc156fa
Daniel Mamani Chino <mjdaniel.2323@gmail.com>
2023-11-14 16:10:54 -0500

    parte 1

--- src/Componentes/Componente.jsx
+++ src/Componentes/Componente.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Componente = () => {
   return (
     <div>
-      <h1>BIENVENIDOS</h1>
       <h1>INSTITUTO SUPERIOR TECNOLOGICO HONORIO DELGADO ESPINOZA</h1>
       <h1>VIVIANA ALLISON RIVERA ANEIRA</h1>
       <h1>TURNO NOCHE</h1>
